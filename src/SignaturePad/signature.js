3d7733e3c9e248d88ddde7b07ddc59bd3a672f28
Parth Vinubhai Gujjar <Pgujjar4826@conestogac.on.ca>
2024-03-02 23:22:59 -0500

    Here i have done the work for the canvas and also for the few button for crud operations...

---
+++ src/SignaturePad/signature.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import SignaturePad from 'react-signature-canvas';
+import '../styles/signature.css';
+
+const Signature = () => {
+    const [sign, setSign] = useState(null);
+
+    // Adjustment of the canvas size 
+
+    function adjustSizes() {
+        const canvas = document.querySelector(".sigCanvas canvas");
+        const canvasContainer = document.querySelector(".sigCanvas");
+
+        const div = document.querySelector(".divcanvas");
+        const divContainer = document.querySelector(".divcanvas");
+
+        if (canvas && canvasContainer) {
+            const ratio = Math.max(window.devicePixelRatio || 1, 1);
+            canvas.width = canvasContainer.offsetWidth * ratio;
+            canvas.height = canvasContainer.offsetHeight * ratio;
+            canvas.getContext("2d").scale(ratio, ratio);
+        }
+
+        if (div && divContainer) {
+            const ratio = Math.max(window.devicePixelRatio || 1, 1);
+            div.style.width = divContainer.offsetWidth * ratio + "px";
+        }
+    }
+
+    // Save the signature
+
+    function saveSignature() {
+        if (sign.isEmpty()) {
+            alert("Please provide a signature first.");
+        } else {
+            const data = sign.toDataURL();
+            console.log(data);
+            alert("Signature saved successfully.");
+        }
+    }
+
+    // New signature canvas
+
+    function newCanvas() {
+        sign.clear();
+    }
+    
+
+    useEffect(() => {
+        adjustSizes();
+
+        window.addEventListener("resize", adjustSizes);
+
+        return () => {
+            window.removeEventListener("resize", adjustSizes);
+        };
+    }, []);
+
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+            <h1>Signature Pad</h1>
+            <div style={{ border: "2px solid black", width: "30%", display: 'flex', justifyContent: 'center' }} className='divcanvas'>
+                <SignaturePad
+                    canvasProps={{ width: 400, height: 200, className: 'sigCanvas' }}
+                    ref={(data) => setSign(data)}
+                />
+            </div>
+            <br />
+            <button onClick={() => { sign.clear() }}> Clear </button>
+            <button onClick = {saveSignature} >Click to Save your sign</button>
+            <button onClick = {newCanvas}>New canvas</button>
+        </div>
+    );
+};
+
+export default Signature;
